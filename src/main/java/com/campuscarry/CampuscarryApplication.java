4a3ef9c0f67e023d5243ad3f17e6a242653e5a63
MeghC <megh.chakravarty@gmail.com>
2026-06-03 07:18:30 +0530

    Initial Commit

---
+++ src/main/java/com/campuscarry/CampuscarryApplication.java
@@ -0,0 +1,13 @@
+package com.campuscarry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscarryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscarryApplication.class, args);
+	}
+
+}
